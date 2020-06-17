6111718c1d9fe1201a1229b22fe764906f874416
Arya18 <aryanpra16dec@gmail.com>
2020-06-17 22:24:17 +0530

    Updates

---
+++ precache-manifest.ede5f54dc6ba70502dc94e3957252e7c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adf3a1701eb0b544a05f0a5a160a54e7",
+    "url": "/Auro/index.html"
+  },
+  {
+    "revision": "f97985a1f1e6e23edd01",
+    "url": "/Auro/static/css/2.bef59309.chunk.css"
+  },
+  {
+    "revision": "1e0cfc35d7d86aec8d52",
+    "url": "/Auro/static/css/4.b4877754.chunk.css"
+  },
+  {
+    "revision": "6bd4de0ecb88a57d3cbd",
+    "url": "/Auro/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "9a72e51ae7e5ac786023",
+    "url": "/Auro/static/js/10.50593ea0.chunk.js"
+  },
+  {
+    "revision": "f0ed32650a92e5711442",
+    "url": "/Auro/static/js/11.2b3fa014.chunk.js"
+  },
+  {
+    "revision": "032403498265b4ece9bf",
+    "url": "/Auro/static/js/12.9a662030.chunk.js"
+  },
+  {
+    "revision": "fc50127e9b60963a00cd",
+    "url": "/Auro/static/js/13.b94d149a.chunk.js"
+  },
+  {
+    "revision": "b88417196e7cf98f62fc",
+    "url": "/Auro/static/js/14.362331fc.chunk.js"
+  },
+  {
+    "revision": "d5075156c991aab4b299",
+    "url": "/Auro/static/js/15.05fe5593.chunk.js"
+  },
+  {
+    "revision": "26eb8c87dc1c20fc75b9",
+    "url": "/Auro/static/js/16.7d8bb914.chunk.js"
+  },
+  {
+    "revision": "fa795853e8a2b8f6adc7",
+    "url": "/Auro/static/js/17.59ea78b5.chunk.js"
+  },
+  {
+    "revision": "dd20e54adae54b73eb64",
+    "url": "/Auro/static/js/18.78fe5d5e.chunk.js"
+  },
+  {
+    "revision": "155079bf810532ce3a7c",
+    "url": "/Auro/static/js/19.a3b455c6.chunk.js"
+  },
+  {
+    "revision": "f97985a1f1e6e23edd01",
+    "url": "/Auro/static/js/2.0430537b.chunk.js"
+  },
+  {
+    "revision": "dd8b6ed0cfc9a2872e0e8e53b5c91dfb",
+    "url": "/Auro/static/js/2.0430537b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5be4598936862336459b",
+    "url": "/Auro/static/js/20.26c38dfd.chunk.js"
+  },
+  {
+    "revision": "778d570b1c87e50e7539",
+    "url": "/Auro/static/js/21.307eabab.chunk.js"
+  },
+  {
+    "revision": "36b9e82c7e39b86e91b8",
+    "url": "/Auro/static/js/22.489690da.chunk.js"
+  },
+  {
+    "revision": "f73ec1713ee2b6938c51",
+    "url": "/Auro/static/js/23.d4568c3d.chunk.js"
+  },
+  {
+    "revision": "368f806e74e052f5752a",
+    "url": "/Auro/static/js/24.6e04fc4f.chunk.js"
+  },
+  {
+    "revision": "b535cb5083d5c63f4507",
+    "url": "/Auro/static/js/3.42f0b94c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Auro/static/js/3.42f0b94c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e0cfc35d7d86aec8d52",
+    "url": "/Auro/static/js/4.342f8902.chunk.js"
+  },
+  {
+    "revision": "66742d732a37a84542ac",
+    "url": "/Auro/static/js/5.f2b95a51.chunk.js"
+  },
+  {
+    "revision": "651a412b738783e448c3",
+    "url": "/Auro/static/js/6.96f069e8.chunk.js"
+  },
+  {
+    "revision": "af48ceba7566e09886e7",
+    "url": "/Auro/static/js/7.27a586eb.chunk.js"
+  },
+  {
+    "revision": "51b46c3a361bb19b158f",
+    "url": "/Auro/static/js/8.b0ff36c7.chunk.js"
+  },
+  {
+    "revision": "f487db226186ed05aec1",
+    "url": "/Auro/static/js/9.9c1620f1.chunk.js"
+  },
+  {
+    "revision": "6bd4de0ecb88a57d3cbd",
+    "url": "/Auro/static/js/main.072a68df.chunk.js"
+  },
+  {
+    "revision": "f50210b8bbc6041cd8ad",
+    "url": "/Auro/static/js/runtime-main.042b035d.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/Auro/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/Auro/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/Auro/static/media/revicons.2feb69cc.eot"
+  }
+]);
